8241986d9ab813ed77e6c76591ffce9e27c16388
Luis Polo <luispolomartinez82@gmail.com>
2018-03-26 00:55:03 -0500

    Primera version pruebas unitarias

---
+++ restful-calculadora/src/main/java/com/devops/calculadora/restfulcalculadora/RestfulCalculadoraApplication.java
@@ -0,0 +1,12 @@
+package com.devops.calculadora.restfulcalculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulCalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulCalculadoraApplication.class, args);
+	}
+}
